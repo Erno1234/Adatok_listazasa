492bb77bfe2c8f465a794dcf201effb4b3a02a11
Erno1234 <paloci.erno@diak.szamalk-szalezi.hu>
2023-03-29 10:35:44 +0200

    Rendezés

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 import{OBJEKTUMLISTA} from "./adatok.js";
 import {rendezesObjektum} from "./rendezes.js";
 $(function(){
-    const articleElem = $("article");
-    tablazatKeszit(OBJEKTUMLISTA, articleElem);
+   init();
+
+    
 });
-function tablazatKeszit(OBJEKTUMLISTA, articleElem){
+function init(){
+  const articleElem = $("article");
+  let tablazat = tablazatKeszit(OBJEKTUMLISTA);
+  articleElem.html(tablazat);
+  const thElem =  $("th");
+  thElem.on("click",function(){
+    let kulcs = $(event.target).attr("id")
+    rendezesObjektum(OBJEKTUMLISTA, kulcs);
+    console.log(OBJEKTUMLISTA);
+    init();
+  })
+}
+function tablazatKeszit(OBJEKTUMLISTA){
     let tablazat = "<table class='table table-striped'>";
     tablazat += " <thead class='table-dark'>";
-    tablazat += "<tr> <th> Név: </th> <th>Kor:</th><th> Fajta: </th></tr>"
+    tablazat += "<tr> <th id='nev'> Név: </th> <th id='kor'>Kor:</th><th id='fajta'> Fajta: </th></tr>"
     tablazat += "</thead>" 
     for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
       tablazat +="<tr>";
       tablazat += "<td>" + OBJEKTUMLISTA[index].nev + "</td>" + "<td>" + OBJEKTUMLISTA[index].kor +"</td>" + "<td>" + OBJEKTUMLISTA[index].fajta +"</td>";
       tablazat +="</tr>";
     }
-    tablazat += "</table>";
-    articleElem.append(tablazat);
+    return tablazat += "</table>";
+    
   }
